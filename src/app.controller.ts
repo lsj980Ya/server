ff6073626a5b61b4c8f1b464f34e492ef91ebf15
mmkj <lsj980ya@mmkj.com>
2024-06-29 17:56:33 +0800

    refine

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(private readonly appService: AppService, private readonly configService: ConfigService,@Inject(databaseConfig.KEY)
+  private dbConfig: ConfigType<typeof databaseConfig>,) {}
+  
   @Get()
   getHello(): string {
+    console.log(this.configService.get('DATABASE_USER'));
+    console.log(this.configService.get('port'));
+    console.log(this.configService.get('database.host'));
+    console.log(this.dbConfig);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/database.config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', `.env.${process.env.NODE_ENV}`],
+      load: [configuration]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
